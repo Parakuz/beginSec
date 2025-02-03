9b1bc342c13d6939600826d78d7be9e1ab574cd9
lnwbeer <beer0817@hotmail.com>
2025-02-03 21:48:14 +0700

    first contact

--- setup/beginsec/app/contact/page.jsx
+++ setup/beginsec/app/contact/page.jsx
@@ -73,7 +73,7 @@ const ContactPage = () => {
                     className="block text-gray-700 text-sm font-bold mb-2"
                     htmlFor="confirm-email"
                   >
-                    Confirm Email
+                    Name
                   </label>
                   <input
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -122,6 +122,7 @@ const ContactPage = () => {
     <Footer/>
     </div>
   );
-};
+}; 
+
 
 export default ContactPage;
