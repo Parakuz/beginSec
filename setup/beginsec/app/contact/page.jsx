004b5e3d29a95840c97b1e7231a110c52aa52263
lnwbeer <beer0817@hotmail.com>
2025-02-27 19:31:39 +0700

    Contact Fix Bug

--- setup/beginsec/app/contact/page.jsx
+++ setup/beginsec/app/contact/page.jsx
@@ -45,22 +45,20 @@ const ContactPage = () => {
         </div>
         <div className="flex flex-col md:flex-row gap-10 mt-8 space-y-8">
           <div className="w-full md:w-1/2 space-y-8">
-              <div className="flex-col justify-start items-start gap-3 inline-flex ">
-                <div className="self-stretch text-white text-[25px] font-bold ">
-                  Send Us A Message
-                </div>
-                <div className="self-stretch text-white text-base font-normal ">
-                  Lorem ipsum dolor sit amet consectetur. Nunc vitae arcu
-                  facilisi nisl cras
-                </div>
+            <div className="flex-col justify-start items-start gap-3 inline-flex ">
+              <div className="self-stretch text-white text-[25px] font-bold ">
+                Send Us A Message
               </div>
-              <form className="space-y-1">
+              <div className="self-stretch text-white text-base font-normal ">
+                Lorem ipsum dolor sit amet consectetur. Nunc vitae arcu facilisi
+                nisl cras
+              </div>
+            </div>
+            <form className="space-y-4">
               <div className="flex mb-4 gap-4">
-                <div className="w-1/2 " >
-                  <label
-                    className="block text-white text-base font-bold mb-2"
-                  >
-                    Name 
+                <div className="w-1/2 ">
+                  <label className="block text-white text-base font-bold mb-2">
+                    Name
                   </label>
                   <input
                     className={`${styles.input_text} shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
@@ -77,26 +75,24 @@ const ContactPage = () => {
                     Email
                   </label>
                   <input
-                    className ={`${styles.input_text} shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+                    className={`${styles.input_text} shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
                     id="confirm-email"
                     type="email"
                     placeholder="Your email"
                   />
                 </div>
               </div>
-              <div className="w-1/2 " >
-                  <label
-                    className="block text-white text-base font-bold mb-2"
-                  >
-                    Name 
-                  </label>
-                  <input
-                    className={`${styles.input_text} shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
-                    id="name"
-                    type="text"
-                    placeholder="Your name"
-                  />
-                </div>
+              <div className="mb-4  ">
+                <label className="block text-white text-base font-bold mb-2">
+                  Phone
+                </label>
+                <input
+                  className={`${styles.input_text} shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+                  id="phone"
+                  type="text"
+                  placeholder="Your phone number"
+                />
+              </div>
               <div className="mb-6">
                 <label
                   className="block text-white text-base font-bold mb-2"
@@ -112,26 +108,25 @@ const ContactPage = () => {
                 ></textarea>
               </div>
               <div className="flex items-center justify-between">
-                <Button text="Send"/>
+                <Button text="Send" />
               </div>
             </form>
           </div>
           <div className="w-full md:w-1/2">
-          <iframe
-  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509374!2d144.9537353153167!3d-37.81627977975195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577d1f9c5f1b1e1!2sFederation%20Square!5e0!3m2!1sen!2sau!4v1633078871234!5m2!1sen!2sau"
-  width="100%"
-  height="400"
-  allowFullScreen=""
-  loading="lazy"
-  className="rounded-md shadow-md"
-></iframe>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509374!2d144.9537353153167!3d-37.81627977975195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577d1f9c5f1b1e1!2sFederation%20Square!5e0!3m2!1sen!2sau!4v1633078871234!5m2!1sen!2sau"
+              width="100%"
+              height="400"
+              allowFullScreen=""
+              loading="lazy"
+              className="rounded-md shadow-md"
+            ></iframe>
           </div>
         </div>
-    </div>
-    <Footer/>
+      </div>
+      <Footer />
     </div>
   );
-}; 
-
+};
 
 export default ContactPage;
